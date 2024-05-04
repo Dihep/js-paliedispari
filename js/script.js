f064e5844268ea51f9e8b0d16722fa8d89d56709
Dihep <dihep.demayo@gmail.com>
2024-05-04 22:38:55 +0200

    Codice di palindromia, funzionante.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Creazione variabili
+const parola = prompt();
+palindroma(parola);
+
+//Funzione palindromia
+function palindroma(p) {
+    let pValue = true;
+    for (let i = 0; 
+        (i < ((p.length)/2)) && (pValue==true); 
+        i++) {
+        if (p[i] == p[(p.length-1-i)]) {
+            pValue = true;
+        } else {
+            pValue = false;
+        };
+    };
+    console.log(`La parola "${parola}" è palindroma? `, pValue);
+};
